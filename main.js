b294c99283dd2ece772bdce21dd777ac068febd2
hirsty777 <777hirsty@gmail.com>
2022-10-10 11:04:21 +0400

    js naw popup text works

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let idCounter=0;
 
 
     
-
+//validate form
 function validateForm(){
   
   if(!validateFirstName()||!validateLastName()||!validateAddres()){
@@ -82,7 +82,7 @@ function showInputValues(){
 
 
     table.innerHTML+=`
-    <tr id="${uI}">
+    <tr id="${uI}" onclick="showNote(this)">
     <td>${idCounter}</td>
     <td>${firstName.value}</td>
     <td>${lastName.value}</td>
@@ -92,10 +92,10 @@ function showInputValues(){
     <td><svg onclick="deleteRow(this)"  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="trash" viewBox="0 0 16 16">
     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
     <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-    </svg></td>
+    </svg></td>  <td>
     <div class="popup-note">
     <div class="note">Note<br>${note.value}</div>
-    </div>
+    </div></td>
     </tr>
     `;
     form.reset();
@@ -107,14 +107,12 @@ function deleteRow(th){
     th.closest('tr').remove();
 };
 
-
-// function showNote(al){
-//   al.closest('div').querySelector('.popup-note').classList.add("active");
-// };
-
-
-
-document.querySelectorAll('.popup-note').onclick=function(e){
-  alert(e);
+//add remove active class on note
+function showNote(noteValue){
+  noteValue.querySelector('.popup-note').classList.toggle('active');
 };
+
+
+
+
 
